e508e9e44ab8bcffa5b1bb0656dbd1a4e27e78ea
Jude Iwuji <105007942+judeiwuji@users.noreply.github.com>
2025-07-07 16:39:50 +0100

    update

---
+++ src/app/proprietor/school-manager/create-school/create-school.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-school',
+  imports: [],
+  templateUrl: './create-school.component.html',
+  styleUrl: './create-school.component.scss'
+})
+export class CreateSchoolComponent {
+
+}
